411851a51245855bf0a282b8ac7ca59922944a7f
Anastasiia Tsyrulnikova <ez53@rambler.ru>
2021-09-27 22:53:14 +0300

    git add .hw02-express

--- model/index.js
+++ model/index.js
@@ -1,16 +1,10 @@
 // const fs = require('fs/promises')
 // const contacts = require('./contacts.json')
-
 const listContacts = async () => {}
-
 const getContactById = async (contactId) => {}
-
 const removeContact = async (contactId) => {}
-
 const addContact = async (body) => {}
-
 const updateContact = async (contactId, body) => {}
-
 module.exports = {
   listContacts,
   getContactById,
