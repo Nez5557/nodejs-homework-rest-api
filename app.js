88059be39de60d6870234c40f6c5287aca698eb9
Anastasiia Tsyrulnikova <ez53@rambler.ru>
2021-10-11 00:12:06 +0300

    git add .hw-auth

--- app.js
+++ app.js
@@ -1,44 +1,50 @@
-// const express = require('express')
-// const logger = require('morgan')
-// const cors = require('cors')
+// const mongoose = require("mongoose");
 
-// const contactsRouter = require('./routes/api/contacts')
+// const DB_HOST = "mongodb+srv://projectnode:Ws13021988@cluster0.hzmwl.mongodb.net/test"
 
-// const app = express()
-
-// const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
-
-// app.use(logger(formatsLogger))
-// app.use(cors())
-// app.use(express.json())
-
-// app.use('/api/contacts', contactsRouter)
-
-// app.use((req, res) => {
-//   res.status(404).json({ message: 'Not found' })
+// mongoose.connect(DB_HOST, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// }).then(() => {
+//   // console.log("Database connect success")
 // })
-
-// app.use((err, req, res, next) => {
-//   const { status = 500, message = 'Server error' } = err
-//   res.status(status).json({
-//     status: 'error',
-//     code: status,
-//     message
+//   .catch (error => {
+//     console.log(error.message);
 //   })
-// })
 
-// module.exports = app
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
 
-const mongoose = require("mongoose");
+const app = express()
 
-const DB_HOST = "mongodb+srv://projectnode:Ws13021988@cluster0.hzmwl.mongodb.net/test"
+const contactsRouter = require('./routes/api/contacts')
+const authRouter = require('./routes/api/auth')
 
-mongoose.connect(DB_HOST, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  // console.log("Database connect success")
-})
-  .catch (error => {
-    console.log(error.message);
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/contacts', contactsRouter)
+app.use('/api/users', authRouter)
+
+app.use((req, res) => {
+  res.status(404).json({
+    status: 'error',
+    code: 404,
+    message: 'Not found',
   })
+})
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = 'Server error' } = err
+  res.status(status).json({
+    status: 'error',
+    code: status,
+    message,
+  })
+})
+
+module.exports = app
