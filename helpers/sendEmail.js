5d7d47541845820f6fbd74a1f95df095c944eb5e
Anastasiia Tsyrulnikova <ez53@rambler.ru>
2021-11-01 20:45:32 +0300

    git add .hw06-email

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,41 +1,41 @@
-const nodemailer = require('nodemailer')
-require('dotenv').config()
-const { EMAIL_PASSWORD } = process.env
+  const nodemailer = require('nodemailer')
+  require('dotenv').config()
+ const { EMAIL_PASSWORD } = process.env
 
-const nodemailerConfig = {
-  host: 'smtp.mail.ru',
-  port: 465, // 25, 465, 2255 нужно убедится в правильности
-  secure: true,
-  auth: {
-    user: 'Nez...5@mail.ru',
-    pass: EMAIL_PASSWORD,
-  },
-}
-
-const transporter = nodemailer.createTransport(nodemailerConfig)
-
-const sendEmail = async data => {
-  try {
-    const email = { ...data, from: nodemailerConfig.auth.user }
-    await transporter.sendEmail(email)
-  } catch (error) {
-    console.log(error)
+  const nodemailerConfig = {
+    host: 'smtp.rambler.ru',
+    port: 465, // 25, 465, 2255 нужно убедится в правильности
+    secure: true,
+    auth: {
+      user: 'ez53@rambler.ru',
+      pass: EMAIL_PASSWORD,
+    },
   }
-}
+
+  const transporter = nodemailer.createTransport(nodemailerConfig)
+
+  const sendEmail = async data => {
+    try {
+      const email = { ...data, from: nodemailerConfig.auth.user }
+       await transporter.sendEmail(email)
+      } catch (error) {
+      console.log(error)
+    }
+  }
 
 module.exports = sendEmail
-
+  
 // const { SENDGRID_KEY } = process.env
 
 // sgMail.setApiKey(SENDGRID_KEY)
 
 // const sendEmail = async(data) => {
-//   const email = {
-//     ...data,
-//     from: 'Nez5557@3g.ua'
-//   }
+//    const email = {
+//      ...data,
+//      from: 'ez53@rambler.ru'
+//    }
 
-//   await sgMail.send(email)
-// }
+//    await sgMail.send(email)
+//   }
 
 // module.exports = sendEmail
